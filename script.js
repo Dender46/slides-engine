c87dd05cba7d9703f68b7a1d8f9e88f8dd8a6afa
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-01-11 01:39:38 +0200

    Add overflow for previews

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const previewPanel = document.querySelector('.preview-panel .container');
 const editor = document.querySelector('.editor');
 let slideCounter = 1
 
+let initialHeight = previewPanel.offsetHeight-2 + 'px';
+
 newSlideBtn.addEventListener("click", function() {
 	document.querySelector('.preview.active').classList.toggle('active');
 	const preview = document.createElement('div');
 		preview.classList.add('preview', 'active');
 		preview.id = slideCounter + 'p';
 	previewPanel.appendChild(preview);
+	
 	document.querySelector('.slide.active').classList.toggle('active');
 	const slide = document.createElement('div');
 		slide.classList.add('slide', 'active');
 		slide.id = slideCounter + 's';
 	editor.appendChild(slide);
 	slideCounter++;
+
+	previewPanel.style.height = initialHeight;
 });
