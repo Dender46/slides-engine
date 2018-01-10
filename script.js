5aca0e4839ba9e1d400a4495a1a9955d6db08f00
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-01-11 00:29:59 +0200

    Init commit
    Can create and highlite active slide.

---
+++ script.js
@@ -0,0 +1,18 @@
+const newSlideBtn = document.querySelector('.new-slide');
+const previewPanel = document.querySelector('.preview-panel .container');
+const editor = document.querySelector('.editor');
+let slideCounter = 1
+
+newSlideBtn.addEventListener("click", function() {
+	document.querySelector('.preview.active').classList.toggle('active');
+	const preview = document.createElement('div');
+		preview.classList.add('preview', 'active');
+		preview.id = slideCounter + 'p';
+	previewPanel.appendChild(preview);
+	document.querySelector('.slide.active').classList.toggle('active');
+	const slide = document.createElement('div');
+		slide.classList.add('slide', 'active');
+		slide.id = slideCounter + 's';
+	editor.appendChild(slide);
+	slideCounter++;
+});
