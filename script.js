9216c6ac51ff3346cc3baaced303550e6bc44806
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-01-12 01:42:14 +0200

    Add scroll function through slides with up/down keys

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const newSlideBtn = document.querySelector('.new-slide');
 const previewPanel = document.querySelector('.preview-panel .container');
 const editor = document.querySelector('.editor');
-let slideCounter = 1
+const previews = previewPanel.children;
+const slides = editor.children;
+let slideCounter = 1;
+let slideCurrIndex = 0;
 
+// setting height for some items
 let initialHeight = previewPanel.offsetHeight-2 + 'px';
 previewPanel.style.height = initialHeight;
 
+
 newSlideBtn.addEventListener("click", function() {
 	slideCounter++;
+	slideCurrIndex++;
 	document.querySelector('.preview.active').classList.toggle('active');
 	const preview = document.createElement('div');
 		preview.classList.add('preview', 'active');
@@ -20,4 +26,25 @@ newSlideBtn.addEventListener("click", function() {
 		slide.dataset.index = slideCounter;
 		slide.innerHTML = `<div class='content'>${slideCounter}</div>`;
 	editor.appendChild(slide);
+});
+
+document.addEventListener('keydown', function(e) {
+	if (e.keyCode == 38) {
+		if (slideCurrIndex == 0)
+			return;	
+		previews[slideCurrIndex].classList.toggle('active');
+		previews[slideCurrIndex-1].classList.toggle('active');
+		slides[slideCurrIndex].classList.toggle('active');
+		slides[slideCurrIndex-1].classList.toggle('active');
+		slideCurrIndex--;
+	}
+	if (e.keyCode == 40) {
+		if (slideCurrIndex == previews.length-1)
+			return;	
+		previews[slideCurrIndex].classList.toggle('active');
+		previews[slideCurrIndex+1].classList.toggle('active');
+		slides[slideCurrIndex].classList.toggle('active');
+		slides[slideCurrIndex+1].classList.toggle('active');
+		slideCurrIndex++;
+	}
 });
