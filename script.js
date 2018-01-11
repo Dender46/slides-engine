7e22129a968334961d8c5362258413ea63d00e04
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-01-11 16:38:43 +0200

    Update .content height from beginning

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ const previewPanel = document.querySelector('.preview-panel .container');
 const editor = document.querySelector('.editor');
 let slideCounter = 1
 
-let initialHeight = previewPanel.offsetHeight-10 + 'px';
+let initialHeight = previewPanel.offsetHeight-2 + 'px';
+previewPanel.style.height = initialHeight;
 
 newSlideBtn.addEventListener("click", function() {
 	slideCounter++;
 	document.querySelector('.preview.active').classList.toggle('active');
 	const preview = document.createElement('div');
 		preview.classList.add('preview', 'active');
-		preview.id = slideCounter + 'p';
+		preview.dataset.index = slideCounter;
 	previewPanel.appendChild(preview);
 	
 	document.querySelector('.slide.active').classList.toggle('active');
 	const slide = document.createElement('div');
 		slide.classList.add('slide', 'active');
-		slide.id = slideCounter + 's';
+		slide.dataset.index = slideCounter;
 		slide.innerHTML = `<div class='content'>${slideCounter}</div>`;
 	editor.appendChild(slide);
-
-	// setting height for some elements
-	previewPanel.style.height = initialHeight;
 });
