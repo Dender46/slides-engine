5bb8b18eebe7071d673224c088490cbaaf1789f3
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-01-12 02:12:10 +0200

    Add function to navigate through slides with preview panel

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-const newSlideBtn = document.querySelector('.new-slide');
+const newSlideBtn  = document.querySelector('.new-slide');
 const previewPanel = document.querySelector('.preview-panel .container');
-const editor = document.querySelector('.editor');
+const editor 	   = document.querySelector('.editor');
 const previews = previewPanel.children;
-const slides = editor.children;
-let slideCounter = 1;
+const slides   = editor.children;
+let slideCounter   = 0;
 let slideCurrIndex = 0;
 
 // setting height for some items
 let initialHeight = previewPanel.offsetHeight-2 + 'px';
 previewPanel.style.height = initialHeight;
 
+function selectSlide() {
+	previews[slideCurrIndex]    .classList.toggle('active');
+	previews[this.dataset.index].classList.toggle('active');
+	slides[slideCurrIndex]    .classList.toggle('active');
+	slides[this.dataset.index].classList.toggle('active');
+	slideCurrIndex = this.dataset.index;
+}
+
+previews[0].addEventListener('click', selectSlide);
 
 newSlideBtn.addEventListener("click", function() {
 	slideCounter++;
-	slideCurrIndex++;
+	slideCurrIndex = slideCounter;
 	document.querySelector('.preview.active').classList.toggle('active');
 	const preview = document.createElement('div');
 		preview.classList.add('preview', 'active');
 		preview.dataset.index = slideCounter;
+		preview.addEventListener('click', selectSlide);
 	previewPanel.appendChild(preview);
+
 	
 	document.querySelector('.slide.active').classList.toggle('active');
 	const slide = document.createElement('div');
@@ -28,22 +39,24 @@ newSlideBtn.addEventListener("click", function() {
 	editor.appendChild(slide);
 });
 
+
+
 document.addEventListener('keydown', function(e) {
 	if (e.keyCode == 38) {
 		if (slideCurrIndex == 0)
 			return;	
-		previews[slideCurrIndex].classList.toggle('active');
+		previews[slideCurrIndex]  .classList.toggle('active');
 		previews[slideCurrIndex-1].classList.toggle('active');
-		slides[slideCurrIndex].classList.toggle('active');
+		slides[slideCurrIndex]  .classList.toggle('active');
 		slides[slideCurrIndex-1].classList.toggle('active');
 		slideCurrIndex--;
 	}
 	if (e.keyCode == 40) {
 		if (slideCurrIndex == previews.length-1)
 			return;	
-		previews[slideCurrIndex].classList.toggle('active');
+		previews[slideCurrIndex]  .classList.toggle('active');
 		previews[slideCurrIndex+1].classList.toggle('active');
-		slides[slideCurrIndex].classList.toggle('active');
+		slides[slideCurrIndex]  .classList.toggle('active');
 		slides[slideCurrIndex+1].classList.toggle('active');
 		slideCurrIndex++;
 	}
