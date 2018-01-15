3037f7680977a1058c56c17bec25e1958aa7ac47
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-01-15 21:23:09 +0200

    Fix bug: can delete first slide and ruin engine

--- script.js
+++ script.js
@@ -20,47 +20,7 @@ function selectSlide(next) {
 	setPageNumber(next+1);
 }
 
-function setPageNumber(num) {
-	document.querySelector('.slide.active footer').textContent = num; 
-}
-
-function deleteSlide() {
-	let index = parseInt(document.querySelector('.slide.active').dataset.index);
-	if (index != 0)
-		selectSlide(index-1);
-	else if (index != slideCounter)
-		selectSlide(index+1);
-	previews[index].remove();
-	slides[index].remove();
-	slideCounter--;
-	for (let i = index; i < previews.length; i++) {
-		previews[i].dataset.index -= 1;
-		slides[i]  .dataset.index -= 1;
-	}
-}
-
-document.addEventListener('keydown', e => {
-	if (e.keyCode == 46) // del key
-		deleteSlide();
-});
-
-function FullScreen() {
-	let width  = document.querySelector('.slide.active').offsetWidth;
-	editor.style.transform = 'scale(1.43)';
-	editor.style.width  = width + 'px';
-	screenfull.request(editor);
-}
-
-screenfull.on('change', () => {
-	if (!screenfull.isFullscreen) {
-		editor.style.width  = '100%';
-		editor.style.transform = 'scale(1)';
-	}
-})
-
-document.querySelector('#fullscr').addEventListener('click', FullScreen);
-
-newSlideBtn.addEventListener("click", function() {
+function addSlide() {
 	slideCounter++;
 	slideCurrIndex = slideCounter;
 	
@@ -86,19 +46,85 @@ newSlideBtn.addEventListener("click", function() {
 			</div>`;
 	editor.appendChild(slide);
 	setPageNumber(slideCurrIndex+1);
-});
+}
+
+function deleteSlide() {
+	let index = slideCurrIndex;
+	if (index != 0)
+		selectSlide(index-1);
+	else if (index != slideCounter) {
+		selectSlide(index+1);
+		slideCurrIndex = 0;
+		setPageNumber(1);
+	} else 
+		return;
+	previews[index].remove();
+	slides[index].remove();
+	slideCounter--;
+	for (let i = index; i < previews.length; i++) {
+		previews[i].dataset.index -= 1;
+		slides[i]  .dataset.index -= 1;
+	}
+}
+
+function FullScreen() {
+	let width  = document.querySelector('.slide.active').offsetWidth;
+	editor.style.transform = 'scale(1.43)';
+	editor.style.width  = width + 'px';
+	screenfull.request(editor);
+}
+
+function setPageNumber(num) {
+	document.querySelector('.slide.active footer').textContent = num; 
+}
+ 
+function addHeader() { 
+	let header = document.querySelector('.preview.active p'); 
+	if (!header) {
+		header = document.createElement('p');
+		header.classList.add('header');
+		document.querySelector('.preview.active').appendChild(header);
+	}
+	header.textContent = this.value;
+}
+
+function addBody() {
+	const body = document.createElement('p');
+	header = document.textContent = this.value;	
+}
+
+screenfull.on('change', () => {
+	if (!screenfull.isFullscreen) {
+		editor.style.width  = '100%';
+		editor.style.transform = 'scale(1)';
+	}
+})
+
+document.querySelector('#fullscr').addEventListener('click', FullScreen);
+
+document.querySelector('.slide.active input').addEventListener('keyup', addHeader);
+
+newSlideBtn.addEventListener("click", addSlide);
 
 document.addEventListener('keydown', function(e) {
-	if (e.keyCode == 38) {
+	if (e.keyCode == 38) { // up key
 		if (slideCurrIndex == 0)
 			return;	
 		selectSlide(slideCurrIndex-1);
 	}
-	if (e.keyCode == 40) {
+	if (e.keyCode == 40) { // down key
 		if (slideCurrIndex == previews.length-1)
 			return;
 		selectSlide(slideCurrIndex+1);
 	}
+	if (e.keyCode == 46) // del key
+		deleteSlide();
+	if (e.keyCode == 107) // plus key
+		addSlide();
+	
+});
+
+document.addEventListener('keydown', e => {
 });
 
 feather.replace();
